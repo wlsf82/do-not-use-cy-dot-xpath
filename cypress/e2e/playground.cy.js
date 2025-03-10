8d1206d04c1174f0c0e47d8396a9d0cf8f36d7be
Walmyr <wlsf82@gmail.com>
2025-03-10 22:35:07 +0100

    Adjust cy.visit command
    There's no need for the slash since the baseUrl already has it.

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -1,7 +1,7 @@
 describe('Cypress Playground', () => {
   it('signs and checks the checkbox to see the signature preview', () => {
     // Arrange
-    cy.visit('/index.html')
+    cy.visit('index.html')
 
     // Act
     cy.get('#signature-textarea-with-checkbox').type('Walmyr')
